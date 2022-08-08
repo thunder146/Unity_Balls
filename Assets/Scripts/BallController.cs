08d7d485a83f87d67b84b6864aa4262d5064c6e6
Igor Pfeifer <i.pfeifer88@gmx.de>
2022-08-08 18:13:27 +0200

    moved code from GameManager to specific BallController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Material RedMaterial;
+    public Material YellowMaterial;
+    public Material BlueMaterial;
+
+    private const float MinScale = 0.1f;
+    private const float MaxScale = 1.5f;
+    private const int MassScaleFactor = 4;
+
+
+    internal void Initialize()
     {
-        
+        var ballScaleRnd = Random.Range(MinScale, MaxScale);
+        var ballScaleTransform = new Vector3(ballScaleRnd, ballScaleRnd, ballScaleRnd);
+
+        SetScale(ballScaleTransform, gameObject);
+        SetMass(ballScaleRnd, gameObject);
+        SetColor(ballScaleRnd, gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void SetScale(Vector3 ballScaleTransform, GameObject ballCreated)
     {
-        
+        ballCreated.transform.localScale = ballScaleTransform;
+    }
+
+    private void SetColor(float ballScaleRnd, GameObject ballCreated)
+    {
+        var ballRenderer = ballCreated.GetComponent<Renderer>();
+
+        if (ballScaleRnd < 0.66)
+        {
+            ballRenderer.material = YellowMaterial;
+        }
+        else if (ballScaleRnd < 1.11)
+        {
+            ballRenderer.material = RedMaterial;
+        }
+        else
+        {
+            ballRenderer.material = BlueMaterial;
+        }
+    }
+
+    private static void SetMass(float ballScaleRandom, GameObject ballCreated)
+    {
+        var rb = ballCreated.gameObject.GetComponent<Rigidbody>();
+        rb.mass = ballScaleRandom * MassScaleFactor;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Timers;
 using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private const float MinScale = 0.1f;
-    private const float MaxScale = 1.5f;
-    private const int MassScaleFactor = 4;
     public GameObject BallPrefab;
 
-    // TODO Move to Ball Controller
-    public Material RedMaterial;
-    public Material YellowMaterial;
-    public Material BlueMaterial;
+    private TextMeshProUGUI _textBallCount;
 
-    private TextMeshProUGUI textBallCount;
-
-    // Start is called before the first frame update
     void Start()
     {
-        textBallCount = GameObject.Find("TextBallCount").GetComponent<TextMeshProUGUI>();
+        _textBallCount = GameObject.Find("TextBallCount").GetComponent<TextMeshProUGUI>();
 
         StartCoroutine(AddBalls(-1, -1, new BallConfig()));
         StartCoroutine(AddBalls(-1, 0, new BallConfig()));
@@ -43,57 +32,28 @@ public class GameManager : MonoBehaviour
 
     private void AddBall(float posXOffset, float posZOffset, BallConfig config)
     {
-        var rndX = Random.Range(-0.1f, 0.1f) + posXOffset;
-        var rndZ = Random.Range(-0.1f, 0.1f) + posZOffset;
+        var posX = Random.Range(-0.1f, 0.1f) + posXOffset;
+        var posY = 10;
+        var posZ = Random.Range(-0.1f, 0.1f) + posZOffset;
 
-        var ballScaleRnd = Random.Range(MinScale, MaxScale);
-        var ballScaleTransform = new Vector3(ballScaleRnd, ballScaleRnd, ballScaleRnd);
-
-        var ballCreated = Instantiate(BallPrefab, new Vector3(rndX, 10, rndZ), Quaternion.identity);
-        SetScale(ballScaleTransform, ballCreated);
-        SetMass(ballScaleRnd, ballCreated);
-        SetColor(ballScaleRnd, ballCreated);
+        var ballCreated = Instantiate(BallPrefab, new Vector3(posX, posY, posZ), Quaternion.identity);
+        var controller = ballCreated.GetComponent<BallController>();
+        controller.Initialize();
 
         if (config.SelfDestroy)
             Destroy(ballCreated, config.SelfDestroySeconds);
     }
 
-    private static void SetScale(Vector3 ballScaleTransform, GameObject ballCreated)
-    {
-        ballCreated.transform.localScale = ballScaleTransform;
-    }
 
-    private void SetColor(float ballScaleRnd, GameObject ballCreated)
-    {
-        var ballRenderer = ballCreated.GetComponent<Renderer>();
-
-        if (ballScaleRnd < 0.66)
-        {
-            ballRenderer.material = YellowMaterial;
-        }
-        else if (ballScaleRnd < 1.11)
-        {
-            ballRenderer.material = RedMaterial;
-        }
-        else
-        {
-            ballRenderer.material = BlueMaterial;
-        }
-    }
-
-    private static void SetMass(float ballScaleRandom, GameObject ballCreated)
-    {
-        var rb = ballCreated.gameObject.GetComponent<Rigidbody>();
-        rb.mass = ballScaleRandom * MassScaleFactor;
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        var balls = GameObject.FindGameObjectsWithTag("Ball");
-        //var balls = this.gameObject.GetComponentsInParent(typeof(BallController));
-        textBallCount.text = $"Ball Count {balls.Length}";
+        UpdateBallCount();
+    }
 
+    private void UpdateBallCount()
+    {
+        var balls = GameObject.FindGameObjectsWithTag("Ball");
+        _textBallCount.text = $"Ball Count {balls.Length}";
     }
 }
 
@@ -101,8 +61,4 @@ class BallConfig
 {
     public bool SelfDestroy = true;
     public float SelfDestroySeconds = 10;
-
-    public BallConfig()
-    {
-    }
 }
